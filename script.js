f1a92ac36d396af7c71798a668c7aa6ff96b6a5c
kanchanr2208 <267316562+kanchanr2208@users.noreply.github.com>
2026-06-23 09:28:43 +0000

    Complete exercise 6 and 7 from loops and arrays TOP exercises. To be Added to the main git files for testing

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-let strings = [
-        "Hare", "Ram", "Hare", "Ram", "Ram", "Ram", "Hare", "Hare", "Hare", 
-        "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"
-    ];
+/*
 
-
-function findUnique(array) {
-    let uniqueArray = [];
-    for (item of array) {
-        if (!uniqueArray.includes(item)) {
-            uniqueArray.push(item);
+//Exercise 06 - repeatString
+const repeatString = function(string, repetitionNumber) {
+    if (typeof string != "string") {
+        return "Error"
+    } else if (typeof repetitionNumber != "number") {
+         return "Error"
+    } else if (((typeof repetitionNumber === "number") && (repetitionNumber < 0)) || !Number.isInteger(repetitionNumber)) {
+        return "Error"
+    } else {
+        let finalString = "";
+        for (let i = 1; i <= repetitionNumber; i++) {
+            finalString = finalString + string;
         }
+       return(finalString);
     }
-    console.log(uniqueArray);
+};
+
+console.log(repeatString("hello", 5));
+
+*/
+
+/*
+//Exercise 07 - Reverse a String
+let string = "hello world";
+
+function reverseString(str) {
+    let reversed = "";
+    for (let i = (str.length - 1); i >= 0; i--) {
+        reversed = reversed + str[i];
+    }
+    return(reversed);
 }
 
-findUnique(strings);
+console.log(reverseString(string));
+*/
