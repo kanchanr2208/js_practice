0750139637db4e78e6bfc90648e78a9256c9c29c
kanchanr2208 <267316562+kanchanr2208@users.noreply.github.com>
2026-06-26 13:31:19 +0000

    more object practice

--- script.js
+++ script.js
@@ -1,112 +1,15 @@
-/*
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 29 };
 
-//Exercise 06 - repeatString
-const repeatString = function(string, repetitionNumber) {
-    if (typeof string != "string") {
-        return "Error"
-    } else if (typeof repetitionNumber != "number") {
-         return "Error"
-    } else if (((typeof repetitionNumber === "number") && (repetitionNumber < 0)) || !Number.isInteger(repetitionNumber)) {
-        return "Error"
-    } else {
-        let finalString = "";
-        for (let i = 1; i <= repetitionNumber; i++) {
-            finalString = finalString + string;
-        }
-       return(finalString);
-    }
-};
+let arr = [ john, pete, mary ];
+let totalAge = 0;
 
-console.log(repeatString("hello", 5));
 
-*/
-
-/*
-//Exercise 07 - Reverse a String
-let string = "hello world";
-
-function reverseString(str) {
-    let reversed = "";
-    for (let i = (str.length - 1); i >= 0; i--) {
-        reversed = reversed + str[i];
-    }
-    return(reversed);
+for (let i = 0; i< arr.length; i++) {
+    totalAge = totalAge + arr[i].age;
 }
 
-console.log(reverseString(string));
-*/
+let averageAge = totalAge/arr.length
 
-/*
-//Exercise 08 - removeFromArray
-
-let array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6] 
-function removeFromArray(arr, ...toBeRemoved) {
-    let filteredArray = arr;
-       for (let i = 0; i < toBeRemoved.length; i++) {
-        filteredArray = filteredArray.filter(item => item != toBeRemoved[i]);
-       }
-       console.log(filteredArray)
-}
-
-removeFromArray(array, 2);
-*/
-
-/*
-//  Exercise 09 - sumAll
-function addEverything(a, b) {
-    let sum = 0;
-    let smaller = 0;
-    let bigger = 0
-    if (a > b) {
-        smaller = b;
-        bigger = a; 
-    } else {
-        smaller = a;
-        bigger = b;
-    }
-    for (let i = smaller; i <= bigger; i++ ) {
-        sum = sum + i;
-    }
-    console.log(sum);
-}
-
-addEverything(5, 1);
-*/
-
-/*
-//Exercise 10 - leapYears
-
-function isLeapYear(a) {
-    if ((a % 400 === 0) || ((a % 4 === 0) && (a % 100 !== 0))) {
-        console.log(a + " is a leap year");
-        return true;
-        
-    } else {
-        console.log(a + " is not a leap year");
-        return false;
-        
-    }
-}
-
-isLeapYear(1900);
-*/
-
-/*
-// Exercise 11 - tempConversion 
-function convertToCelsius(a) {
-    let conversion = ((a - 32) * (5/9));
-    let final = Number(conversion.toFixed(1));
-    console.log(final); 
-   
-}
-
-function convertToFahrenheit(a) {
-    let conversion = ((a * (9/5)) + 32);
-    let final = Number(conversion.toFixed(1));
-    console.log(final);
-}
-
-convertToCelsius(100);
-convertToFahrenheit(99);
-*/
-
+console.log(averageAge);
